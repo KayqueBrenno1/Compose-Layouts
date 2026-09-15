2097c70699e796b4884b5c2423deb93e2ff4686e
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@dsn127m1246732.cfp127aluno.local>
2026-09-15 13:44:26 -0300

    feat: iniciar compose layouts

---
+++ app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.composelayouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composelayouts.ui.theme.ComposeLayoutsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeLayoutsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding).background(Color(0xFF9D00FF))
+                    ) {
+                        Text("Aula Android", color = Color.White)
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeLayoutsTheme {
+        Greeting("Android")
+    }
+}
