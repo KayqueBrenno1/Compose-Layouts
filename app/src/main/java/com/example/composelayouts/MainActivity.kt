db8a69efcfd17d5e8dd90a96966c4565424c92b9
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@dsn127m1246732.cfp127aluno.local>
2026-09-15 14:18:03 -0300

    feat: enviar Box

--- app/src/main/java/com/example/composelayouts/MainActivity.kt
+++ app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -8,12 +8,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelayouts.ui.theme.ComposeLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,9 +27,21 @@ class MainActivity : ComponentActivity() {
             ComposeLayoutsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(
-                        modifier = Modifier.padding(innerPadding).background(Color(0xFF9D00FF))
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .background(Color(0xFF9D00FF))
+                            .size(
+                                width = 200.dp,
+                                height = 300.dp
+                            ),
+                        contentAlignment = Alignment.Center
                     ) {
                         Text("Aula Android", color = Color.White)
+                        Text(
+                            text = "Com Jetpack Compose",
+                            modifier = Modifier.align(Alignment.BottomCenter),
+                            color = Color.White
+                        )
 
                     }
                 }
